ab8af6b91a26d7618139c29405fe3009a67a94a4
201930425 <no012010@email.daelim.ac.kr>
2025-04-17 11:29:47 +0900

    v1.9 add week07.txt

--- week07_kiosk.py
+++ week07_kiosk.py
@@ -13,17 +13,28 @@ def order_process(idx):
     total_price = total_price + prices[idx]
     amounts[idx] = amounts[idx] + 1
 
-def display_menu(ids):
+def display_menu():
     """
     음료 선택 메뉴 디스플레이 기능
-    :param ids: 고객이 선택한 메뉴 -1(인텍스)
+    :return:
+    """
+    print("=" * 30)
+    menu_texts = "".join([f"{j + 1}) {drinks[j]} {prices[j]}원\n" for j in range(len(drinks))])
+    menu_texts = menu_texts + f"{len(drinks) + 1}) 주문종료 : "
+    return menu_texts
+
+def print_receipt():
+    """
+    영수증 출력기능
     :return: 없음
     """
-menu_texts = "".join([f"{j+1}) {drinks[j]} {prices[j]}원 "for j in range(len(drinks))])
-menu_texts = menu_texts + f"{len(drinks)+1}) 주문종료 :  "
-
+    print(f"{'상품명':^20}{'단가':^6}{'수량':^6}{'금액':^6}")
+    for i in range(len(drinks)):
+        if amounts[i] > 0:
+            print(f"{drinks[i]:^20}{prices[i]:^6}{amounts[i]:^6}{prices[i] * amounts[i]:^6}")
+    print(f"총 주문 금액 : {total_price}원")
 while True:
-    menu =int(input(menu_texts))
+    menu =int(input(display_menu()))
     if len(drinks)>= menu >= 1:
         order_process(menu-1)
     elif menu == len(drinks)+1:
@@ -32,8 +43,4 @@ while True:
     else:
         print(f"{menu}번 메뉴는 존재하지 않습니다. 아래 메뉴에서 골라주세요")
 
-print(f"{'상품명':^20}{'단가':^6}{'수량':^6}{'금액':^6}")
-for i in range(len(drinks)):
-    if amounts[i] > 0:
-        print(f"{drinks[i]:^20}{prices[i]:^6}{amounts[i]:^6}{prices[i] * amounts[i]:^6}")
-print(f"총 주문 금액 : {total_price}원")
+print_receipt()
