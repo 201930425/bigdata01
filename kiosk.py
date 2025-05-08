72b4e5517ddaf77c927959575366e35788076968
201930425 <no012010@email.daelim.ac.kr>
2025-05-08 10:39:48 +0900

    v2.6 kiosk

--- kiosk.py
+++ kiosk.py
@@ -3,7 +3,13 @@ prices = [1500,2500, 4000, 4200]
 total_price = 0
 amounts = [0] * len(drinks)
 
-
+def apply_discount(price: int) -> float:
+    """
+    총 금액이 특정 금액(임계값)을 넘어서면 할인율 적용 함수
+    :param price: 할인 전 총 금액
+    :return: 할인이 적용된 금액 또는 할인이 적용되지 않는 금액
+    """
+    pass
 
 def order_process(idx :int) -> None:
     """
